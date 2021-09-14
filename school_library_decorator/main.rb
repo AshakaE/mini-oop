94dbb90ed45d4f838f8bc077fdfbdb6b5a6f6cb2
Arslan Bisharat <thearslan380@gmail.com>
2021-09-13 23:41:43 -0700

    Added expected lines between methods

--- school_library_decorator/main.rb
+++ school_library_decorator/main.rb
@@ -1,27 +1,27 @@
 # To show all books all_books method:
 def all_books
-
 end
+
 # To list all people all_people method:
 def all_people
-
 end
+
 # To create person create_person method:
 def create_person
-
 end
+
 # To Create a book create_book method:
 def create_book
-
 end
+
 # To Create a rental create_book method:
 def create_renatl
-
 end
+
 # To show all rentals all_rentals method:
 def all_rentals
-
 end
+
 # To invoke main.rb
 def main
     puts 'Hello there,'
