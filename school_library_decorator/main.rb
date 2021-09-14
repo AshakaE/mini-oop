d96a80068727b0420f8b6919c63596754c1ca676
Arslan Bisharat <thearslan380@gmail.com>
2021-09-13 23:46:09 -0700

    Added 2 instead of 4 indentations

--- school_library_decorator/main.rb
+++ school_library_decorator/main.rb
@@ -18,20 +18,20 @@ def all_rentals; end
 
 # To invoke main.rb
 def main
-    puts 'Hello there,'
-    puts 'Welcome to Basic UI of School Libraray App!'
-    puts 'Please choose an option by entering a number:'
-    puts '1- List all books'
-    puts '2- List all People'
-    puts '3- Create a Person'
-    puts '4- Create a Book'
-    puts '5- Create a Rental'
-    puts '6- List all Rentals for a given Person id'
-    puts '7- Exit'
-    puts ' '
-    puts 'Do you want to Create a Student (1) or a teacher (2)?'
-    number = gets.chomp
-    puts number
+  puts 'Hello there,'
+  puts 'Welcome to Basic UI of School Libraray App!'
+  puts 'Please choose an option by entering a number:'
+  puts '1- List all books'
+  puts '2- List all People'
+  puts '3- Create a Person'
+  puts '4- Create a Book'
+  puts '5- Create a Rental'
+  puts '6- List all Rentals for a given Person id'
+  puts '7- Exit'
+  puts ' '
+  puts 'Do you want to Create a Student (1) or a teacher (2)?'
+  number = gets.chomp
+  puts number
 end
 
 main
